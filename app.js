338716ba3719bf2f9e7356fdaa2b01d120291a6a
Federico <federico.valeri@studenti.unicam.it>
2018-06-27 12:06:08 +0200

    mongolab connected

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 var MongoStore = require('connect-mongo')(session);
 var config = require('config.json')('./config.json');
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+
 
 
 
@@ -39,29 +38,13 @@ var app = express();
 
 //connect to database
 //mongoose.connect(config.mongodb.db_url);
-
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-
-//(Focus on This Variable)
-var url = process.env.MONGOLAB_URI;
-//(Focus on This Variable)
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        console.log('Connection established to', url);
-
-        // do some work here with the database.
-
-        //Close connection
-        db.close();
-    }
+var url = "mongodb://fede:fe18de11@ds119651.mlab.com:19651/league-shop";
+mongoose.connect(url, function(error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
 });
+
+
 
 require('./config/passport');
 
